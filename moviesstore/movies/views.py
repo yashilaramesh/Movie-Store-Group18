0628b7a189ddcc0da4c34a3e60367a4f8b5e9bd0
kamya-rajesh <kam.r2576@gmail.com>
2025-02-11 16:17:04 -0500

    search fixed

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -14,7 +14,7 @@ def index(request):
         movies = Movie.objects.all()
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
+    template_data['movies'] = movies
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
 def show(request, id):
