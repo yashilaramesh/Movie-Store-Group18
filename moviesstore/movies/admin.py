3e9d9f4fdd7cef1cf484f7277323e91691a3be83
kamya-rajesh <kam.r2576@gmail.com>
2025-02-11 16:24:37 -0500

    admin movie search bug fix

--- moviesstore/movies/admin.py
+++ moviesstore/movies/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Movie, Review
-admin.site.register(Movie)
+
+
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
+
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
