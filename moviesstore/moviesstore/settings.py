4e0d348720cb35641831ecfa8b8164a73d08b901
Yashila Ramesh <yashiramesh1122@gmail.com>
2025-02-12 17:45:16 -0500

    moviesstore static branch set

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -129,5 +129,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # made change here by Vidhi
 STATICFILES_DIRS = [
-    BASE_DIR / 'moviesstore/static/',
+    BASE_DIR / 'moviesstore/moviesstore/static/',
 ]
