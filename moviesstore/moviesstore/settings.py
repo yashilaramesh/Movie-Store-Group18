39d5c2e645efc9f1698cacbabd713b3f68acf339
Yashila Ramesh <53922713+yashilaramesh@users.noreply.github.com>
2025-02-12 17:58:16 -0500

    Update settings.py

--- moviesstore/moviesstore/settings.py
+++ moviesstore/moviesstore/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-k)s$hcpooz^3kh$)$&y6zl7erkuki^w5upqx*&wj8d9gs)-qk4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
